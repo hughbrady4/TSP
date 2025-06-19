24442afd4611f0c6d19284fc2e20223e0a8b43b4
Hugh Brady <hugh@organicsystemsllc.com>
2025-06-19 08:18:22 -0400

    Latest project state

---
+++ app/src/main/java/com/organicsystemsllc/travelingsalesman/MainActivity.java
@@ -0,0 +1,361 @@
+package com.organicsystemsllc.travelingsalesman;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.icu.text.DateFormat;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+//import androidx.navigation.NavController;
+//import androidx.navigation.Navigation;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.room.Room;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.annotations.Nullable;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.organicsystemsllc.travelingsalesman.databinding.ActivityMainBinding;
+import com.organicsystemsllc.travelingsalesman.ui.login.UserViewModel;
+import com.organicsystemsllc.travelingsalesman.ui.login.UserData;
+import com.organicsystemsllc.travelingsalesman.ui.maps.MapsViewModel;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    private UserViewModel mUserViewModel;
+    public static final String TAG = "TRAVELING_SALESMAN";
+//    public final AppDatabase mDb = Room.databaseBuilder(getApplicationContext(),
+//            AppDatabase.class, "traveling-salesman-db").build();
+    private final ActivityResultLauncher<Intent> mSignInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            this::onSignInResult
+    );
+    private FusedLocationProviderClient mFusedLocationClient;
+    private ActivityResultLauncher<String[]> mLocationPermissionRequest;
+    private PendingIntent mPendingIntent;
+    private MapsViewModel mMapsViewModel;
+    //    private Fused mFusedLocationClient;
+
+//    private LocationCallback mLocationCallback;
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            assert user != null;
+            mUserViewModel.getUser().setValue(user);
+            Toast.makeText(this, "User sign in.", Toast.LENGTH_SHORT).show();
+
+        } else {
+            //some error logging
+            mUserViewModel.getUser().setValue(null);
+            if (response != null && response.getError() != null
+                    && response.getError().getLocalizedMessage() != null) {
+                Log.e(TAG, response.getError().getLocalizedMessage());
+                Toast.makeText(this, response.getError().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+    private void signIn() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+//                new AuthUI.IdpConfig.FacebookBuilder().build());
+//                new AuthUI.IdpConfig.TwitterBuilder().build());
+
+        // Create and launch sign-in intent
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.mipmap.ic_launcher)
+                .setTheme(R.style.Theme_TravelingSalesman)
+                .build();
+        mSignInLauncher.launch(signInIntent);
+    }
+
+    private void signOut() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(task -> mUserViewModel.getUser().setValue(null));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_sign_in) {
+            signIn();
+            return true;
+        } else if (id == R.id.action_sign_out) {
+            signOut();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityMainBinding mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        // Initialize Firebase Auth
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.addAuthStateListener(firebaseAuth -> {
+            FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+            if(currentUser != null){
+                mUserViewModel.getUser().setValue(currentUser);
+                mUserViewModel.getPhotoUri().setValue(currentUser.getPhotoUrl());
+                getUserData(currentUser);
+
+            } else {
+                mUserViewModel.getUser().setValue(null);
+                mUserViewModel.getPhotoUri().setValue(null);
+            }
+
+        });
+
+
+        mUserViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+
+//        mMapsViewModel = new ViewModelProvider(this).get(MapsViewModel.class);
+        mUserViewModel.getTrackToggle().observeForever(new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean track) {
+                Log.i(TAG, "Track indicator updated: " + track.toString());
+                if (Boolean.TRUE.equals(track)) {
+                    startLocationUpdates();
+                } else {
+                    stopLocationUpdates();
+                }
+            }
+        });
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_maps, R.id.navigation_routes, R.id.navigation_notifications)
+                .build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(navView, navController);
+
+
+        //Location permission request
+        mLocationPermissionRequest = registerForActivityResult(new ActivityResultContracts
+                .RequestMultiplePermissions(), result -> {
+
+            Boolean fineGranted = result
+                    .getOrDefault(android.Manifest.permission.ACCESS_FINE_LOCATION,
+                            false);
+            Boolean coarseGranted = result
+                    .getOrDefault(android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                            false);
+
+            Boolean backgroundGranted = result
+                    .getOrDefault(Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                            false);
+            if (Boolean.FALSE.equals(backgroundGranted)) {
+                return;
+            }
+
+            if (Boolean.TRUE.equals(fineGranted) || Boolean.TRUE.equals(coarseGranted)) {
+                startLocationUpdates();
+            }
+        });
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+    }
+
+    private void getUserData(FirebaseUser currentUser) {
+        DocumentReference docRef = FirebaseFirestore.getInstance().collection("users").document(currentUser.getUid());
+
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        UserData data = document.toObject(UserData.class);
+
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        if (data != null) {
+                            Boolean online = data.isOnline();
+                            Boolean tracking = data.isOnline();
+//                            Timestamp updated = data.getUpdated();
+
+                            mUserViewModel.getOnlineToggle().setValue(online);
+                            mUserViewModel.getTrackToggle().setValue(tracking);
+//                            mUserViewModel.getText().setValue(updated.toString());
+                        }
+
+
+
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w(TAG, "Listen failed.", e);
+                    return;
+                }
+
+                String source = snapshot != null && snapshot.getMetadata().hasPendingWrites()
+                        ? "Local" : "Server";
+
+                if (snapshot != null && snapshot.exists() && source.equalsIgnoreCase("server")) {
+                    Log.d(TAG, source + " data: " + snapshot.getData());
+                    UserData data = snapshot.toObject(UserData.class);
+                    if (data != null) {
+//                        Boolean online = data.isOnline();
+//                        Boolean tracking = data.isOnline();
+                        Timestamp updated = data.getUpdated();
+//                        mMapsViewModel.getOnlineToggle().setValue(online);
+//                        mMapsViewModel.getTrackToggle().setValue(tracking);
+                        String dateString = DateFormat.getDateTimeInstance().format(updated.toDate());
+                        mUserViewModel.getText().setValue(dateString);
+                    }
+                } else {
+                    Log.d(TAG, source + " data: null");
+                }
+            }
+        });
+
+    }
+
+    private void startLocationUpdates() {
+
+        int finePermission = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        int coursePermission = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+
+        int version = Build.VERSION.SDK_INT;
+        int versionBG = Build.VERSION_CODES.Q;
+        Log.i(TAG, Integer.toString(version) + ", " + versionBG);
+        int backgroundPermission = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION);
+        if (backgroundPermission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, Integer.toString(finePermission));
+            Log.i(TAG, Integer.toString(coursePermission));
+            Log.i(TAG, Integer.toString(backgroundPermission));
+
+            mLocationPermissionRequest.launch(new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+            });
+        } else {
+            Log.i(TAG, "Has background location permission");
+        }
+
+        LocationRequest locationRequest = new LocationRequest.Builder(10000)
+                .setDurationMillis(Long.MAX_VALUE)
+                .setMinUpdateDistanceMeters(1)
+                .build();
+//        mFusedLocationClient.requestLocationUpdates(locationRequest,
+//                mLocationCallback,
+//                Looper.getMainLooper());
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser == null) {
+            return;
+        }
+
+        Intent intent = new Intent(this, LocationReceiver.class);
+        intent.putExtra("com.organicsystemsllc.travelingsalesman.DATA", currentUser.getUid());
+        intent.setAction("com.organicsystemsllc.travelingsalesman.ACTION_UPDATE_DATA");
+
+        mPendingIntent = PendingIntent
+                .getBroadcast(this, 0, intent, PendingIntent.FLAG_MUTABLE);
+
+        mFusedLocationClient.requestLocationUpdates(locationRequest, mPendingIntent);
+
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mPendingIntent);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/organicsystemsllc/travelingsalesman/ui/maps/MapsFragment.java
@@ -0,0 +1,266 @@
+package com.organicsystemsllc.travelingsalesman.ui.maps;
+
+import static com.organicsystemsllc.travelingsalesman.MainActivity.TAG;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.AdvancedMarker;
+import com.google.android.gms.maps.model.AdvancedMarkerOptions;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapCapabilities;
+import com.google.android.gms.maps.model.PinConfig;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.organicsystemsllc.travelingsalesman.MainActivity;
+import com.organicsystemsllc.travelingsalesman.R;
+import com.organicsystemsllc.travelingsalesman.databinding.FragmentMapsBinding;
+
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+public class MapsFragment extends Fragment implements
+        GoogleMap.OnCameraMoveStartedListener,
+        GoogleMap.OnCameraMoveListener,
+        GoogleMap.OnCameraMoveCanceledListener,
+        GoogleMap.OnCameraIdleListener,
+        GoogleMap.OnMyLocationClickListener {
+
+    private GoogleMap mMap;
+    private final OnMapReadyCallback mCallback = new OnMapReadyCallback() {
+
+        @Override
+        public void onMapReady(GoogleMap map) {
+            mMap = map;
+            MapCapabilities capabilities = map.getMapCapabilities();
+            Log.i(TAG, "Advanced marker enabled? " + capabilities.isAdvancedMarkersAvailable());
+
+//            mMap.setOnCameraIdleListener();
+//            mMap.setOnCameraMoveStartedListener(this);
+//            mMap.setOnCameraMoveListener(this);
+//            mMap.setOnCameraMoveCanceledListener(this);
+
+            setLocationEnabled();
+            map.setOnMyLocationClickListener(MapsFragment.this);
+
+        }
+    };
+    private MapsViewModel mMapsViewModel;
+    private final LinkedList<MapNode> mMapNodes = new LinkedList<>();
+    private final ArrayList<Polyline> mMapEdges = new ArrayList<>();
+    private static final char[] LABELS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    private FragmentMapsBinding mMapsBinding;
+    private final FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
+    private ActivityResultLauncher<String[]> mLocationPermissionRequest;
+    private FragmentMapsBinding mBinding;
+
+
+    private void addMarkerToMap(LatLng position, @NonNull GoogleMap map) {
+        int index = mMapNodes.size();
+        String label = String.valueOf(LABELS[index % LABELS.length]);
+        // Set the glyph text.
+
+        PinConfig pinConfig = PinConfig.builder()
+                .setBackgroundColor(Color.RED)
+                .setBorderColor(Color.GREEN)
+                .setGlyph(new PinConfig.Glyph(label))
+                .build();
+
+        AdvancedMarkerOptions options = new AdvancedMarkerOptions()
+                .icon(BitmapDescriptorFactory.fromPinConfig(pinConfig))
+                .title(label)
+                .position(position);
+
+        AdvancedMarker marker = (AdvancedMarker) map.addMarker(options);
+        
+        assert marker != null;
+        marker.setDraggable(true);
+
+
+        MapNode node = new MapNode(position, label, false);
+//        mMapNodes.add(node);
+//        mMapsViewModel.getMapNodes().setValue(mMapNodes);
+        marker.setTag(node);
+//        reverseGeoCode(marker, node);
+        //add to firebase
+//        FirebaseUser user = mAuth.getCurrentUser();
+//        if (user != null) {
+//            mDatabase.child("user_nodes").child(user.getUid()).setValue(mMapNodes);
+//        }
+//        addToDatabase(node);
+
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        mBinding = FragmentMapsBinding.inflate(inflater, container, false);
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(mCallback);
+        }
+
+//        FloatingActionButton fab;
+        Button addBtn = mBinding.addNode;
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null) {
+            Toast.makeText(getContext(),"Please login to publish location.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("latitude", location.getLatitude());
+        userMap.put("longitude", location.getLongitude());
+        userMap.put("altitude", location.getAltitude());
+        userMap.put("bearing", location.getBearing());
+        userMap.put("updated", FieldValue.serverTimestamp());
+
+        FirebaseFirestore.getInstance().collection("users").document(currentUser.getUid())
+            .set(userMap, SetOptions.merge())
+            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Snackbar.make(requireView(), "Location updated on server.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    Log.d(MainActivity.TAG, "DocumentSnapshot successfully written!");
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Snackbar.make(requireView(), "Failed to update server.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    Log.w(MainActivity.TAG, "Error writing document", e);
+                }
+            });
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Initialize Firebase Auth
+
+        mMapsViewModel = new ViewModelProvider(requireActivity()).get(MapsViewModel.class);
+
+        mLocationPermissionRequest = registerForActivityResult(new ActivityResultContracts
+                .RequestMultiplePermissions(), result -> {
+
+            Boolean fineGranted = result
+                    .getOrDefault(android.Manifest.permission.ACCESS_FINE_LOCATION,
+                            false);
+            Boolean coarseGranted = result
+                    .getOrDefault(android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                            false);
+
+            Log.i(TAG,"Request Permission Result: " + fineGranted + ", " + coarseGranted);
+
+            if (fineGranted != null && fineGranted) {
+                setLocationEnabled();
+            } else if (coarseGranted != null && coarseGranted ) {
+                setLocationEnabled();
+            }
+        });
+    }
+
+    private void setLocationEnabled() {
+        int fineGranted = ActivityCompat
+                .checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+        int courseGranted = ActivityCompat
+                .checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        Log.i(TAG, "Location: " + fineGranted + ", " + PackageManager.PERMISSION_GRANTED);
+        if (fineGranted != PackageManager.PERMISSION_GRANTED &&
+                courseGranted != PackageManager.PERMISSION_GRANTED) {
+
+            Log.i(TAG,"Request Permission!");
+            mLocationPermissionRequest.launch(new String[] {
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+
+            });
+            return;
+        }
+        if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        }
+    }
+
+    @Override
+    public void onCameraIdle() {
+
+    }
+
+    @Override
+    public void onCameraMoveCanceled() {
+
+    }
+
+    @Override
+    public void onCameraMove() {
+
+    }
+
+    @Override
+    public void onCameraMoveStarted(int i) {
+
+    }
+}
